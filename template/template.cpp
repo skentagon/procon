0d27a8be125608bc0b154ca33e43b48c09b20660
skentagon <kentaro23313@zeus.eonet.ne.jp>
2019-11-02 06:16:05 +0900

    modefy template

--- template/template.cpp
+++ template/template.cpp
@@ -1,11 +1,12 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-namespace skentagon::util{};
+namespace skentagon::util{}
 using namespace skentagon::util;
 
 typedef long long ll;
 #define rep(i,n) for( int (i)=0; (i)<(n); ++(i) )
+#define rpv(d) for(auto& v:d)
 #define co(n) cout << (n) << endl;
 #define cn(n) cin >> n;
 #define all(x) (x).begin(), (x).end()
