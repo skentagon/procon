8c359fc72667733562ae68e71ebe37b24e6c2abe
skentagon <kentaro23313@zeus.eonet.ne.jp>
2019-12-09 10:40:13 +0900

    add ABC147

---
+++ AtCoder/ABC147/b.cpp
@@ -0,0 +1,34 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for( ll (i)=0; (i)<(n); ++(i) )
+#define rpi(i,n) for( int (i)=0; (i)<(n); ++(i) )
+#define rpv(d) for(auto& v:d)
+#if __cplusplus < 201703L
+  std::ostream& co_(){ return std::cout << std::endl; }
+  std::istream& cn_(){ return std::cin; }
+  template<class Head, class... Args>
+  std::ostream& co_( Head&& head, Args&&... args ){ std::cout << head; return co_(std::forward<Args>(args)...); }
+  template<class Head, class... Args>
+  std::istream& cn_( Head&& head, Args&&... args ){ std::cin >> head; return cn_(std::forward<Args>(args)...); }
+#else
+  template<class... Args>
+  std::ostream& co_( Args... args ){ return std::cout << ... << args << std::endl; }
+  template<class... Args>
+  std::istream& cn_( Args... args ){ return std::cin >> ... >> args; }
+#endif
+#define co(...) co_(__VA_ARGS__);
+#define cn(...) cn_(__VA_ARGS__);
+#define all(x) (x).begin(), (x).end()
+#define rgt(x,i) (x).begin(), (x).begin()+(i)
+
+int main(){
+  string s;cn(s)
+  int ans = 0;
+  rpi(i,s.size()/2){
+    if ( s[i] != s[s.size()-1-i] )++ans;
+  }
+  co(ans)
+}
